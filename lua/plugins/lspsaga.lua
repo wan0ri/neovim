eb92ce99f5c0a51d1ea7ed7fc1bc06c9af7495f9
wan0ri <p.t.c_vdl_blue@icloud.com>
2026-02-08 18:06:18 +0900

    fix: 諸々の設定見直し

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -5,5 +5,7 @@ return {
 	opts = {
 		lightbulb = { enable = false },
 		outline = { auto_open = false },
+		-- Neovim 0.12 以降の非推奨API警告を避けるため symbol 機能を明示無効
+		symbol = { enable = false },
 	},
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,12 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
+  "nvim-telescope/telescope.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  keys = {
+    {"<C-p>", function() require("telescope.builtin").find_files() end, desc = "Quick Open (files)"},
+    {"<leader>fa", function() require("telescope.builtin").find_files({ no_ignore = true, hidden = true }) end, desc = "Find files (ALL: hidden & gitignored)"},
+    {"<leader>ff", function() require("telescope.builtin").find_files() end, desc = "Find files"},
+    {"<leader>fg", function() require("telescope.builtin").live_grep() end, desc = "Search in files"},
+    {"<leader>fb", function() require("telescope.builtin").buffers() end, desc = "Find buffers"},
+    {"<leader>sp", function() require("telescope.builtin").commands() end, desc = "Command Palette"},
+  },
 }
